594a9eb98a44d278a43ff2f768ba455aafcd012c
nick <yannick.katambo@outlook.com>
2020-11-18 23:37:08 +0200

    progress commit

--- DataProcessor/CsvFileProcessor.cs
+++ DataProcessor/CsvFileProcessor.cs
@@ -1,4 +1,9 @@
-﻿namespace DataProcessor
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DataProcessor
 {
     internal class CsvFileProcessor
     {
@@ -13,7 +18,20 @@
 
         public void Process()
         {
+            using (StreamReader input = File.OpenText(InputFilePath))
+            using(CsvReader csvReader = new CsvReader((IParser)input))
+            {
+                IEnumerable<dynamic> records = csvReader.GetRecord<dynamic>();
+                
+                foreach (var record in records)
+                {
+                    Console.WriteLine(record.OrderNumber);
+                    Console.WriteLine(record.CustomerNumber);
+                    Console.WriteLine(record.Description);
+                    Console.WriteLine(record.Quatity);
 
+                }
+            }
         }
     }
 }
